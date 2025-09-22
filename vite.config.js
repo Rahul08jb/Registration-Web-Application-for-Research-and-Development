de9dc76fc2329fc06820de282de97de2ca8ed0ff
Rahul08jb <rahulkumar.sagar1818@gmail.com>
2025-09-22 23:23:46 +0530

    base

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Registration-Web-Application-for-Research-and-Development/',
   plugins: [react()],
   optimizeDeps: {
     include: ['swiper'],
-    base: '/Registration-Web-Application-for-Research-and-Development/',
+   
 
   
   },
